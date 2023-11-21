ab86876632886feae543510fe46928bbb64eb2b8
Bella <kbee1202@gmail.com>
2023-11-21 11:23:01 +0900

    no message

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { Grid } from '@mui/material';
+import { Grid, Container } from '@mui/material';
 
 function Login() {
   return (
-    <div>
+    <Container maxWidth="sm" sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
       Login
-      <Grid container>
-        <Grid item xs={12}>
-          a
-        </Grid>
-        <Grid item xs={12}>
-          b
-        </Grid>
+      <Grid item xs={12}>
+        a
       </Grid>
-    </div>
+      <Grid item xs={12}>
+        b
+      </Grid>
+    </Container>
   );
 }
 export default Login;
